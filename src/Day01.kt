1b554db192899f69172d38468719721e2674bb57
gustxvo <gualmeida2004@gmail.com>
2024-12-04 18:11:08 -0300

    Day 01 - Solution 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,41 @@
+import kotlin.math.abs
+
 fun main() {
+
+    fun locationLists(input: List<String>): Pair<List<Int>, List<Int>> {
+        val l1 = mutableListOf<Int>()
+        val l2 = mutableListOf<Int>()
+        input.forEach { line ->
+            val a = line.substringBefore(" ").toInt()
+            val b = line.substringAfterLast(" ").toInt()
+            l1.add(a)
+            l2.add(b)
+        }
+        return l1 to l2
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (l1, l2) = locationLists(input)
+        return l1.sorted().zip(l2.sorted())
+            .sumOf { (a, b) -> abs(a - b) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (l1, l2) = locationLists(input)
+        val elementCount = mutableMapOf<Int, Int>()
+        l1.forEach {
+            elementCount.put(it, l1.count { i -> i == it })
+        }
+
+        val elementScore = l1.associate { first ->
+            first to l2.count { first == it }
+        }
+
+        return elementScore.entries.sumOf { (k, v) ->
+            k * v * elementCount.getOrDefault(k, 0)
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
