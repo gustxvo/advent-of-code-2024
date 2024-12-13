8d71441c3f5cb3b65687f3ceb691c2c97b778e6a
gustxvo <gualmeida2004@gmail.com>
2024-12-13 00:53:14 -0300

    Day 08 - Solution 3

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,3 +1,9 @@
+private sealed interface Strategy
+
+private data object Signal : Strategy
+
+private data object Resonance : Strategy
+
 fun main() {
 
     data class Vec2(val x: Int, val y: Int)
@@ -19,48 +25,50 @@ fun main() {
         }
     }
 
-    fun antiNodesLocationsInMap(
-        map: List<String>,
-        antiNodesLocationsForFrequency: (Vec2, Vec2) -> Set<Vec2>,
-    ): Int = frequencyLocationsMap(map)
-        .flatMap { (_, positions) ->
-            val antiNodesPositions = buildSet<Vec2> {
-                positions.dropLast(1).forEachIndexed { index, currentPosition ->
-                    val subsequentFrequencyPositions = positions.slice(index + 1..positions.lastIndex)
-                    subsequentFrequencyPositions.forEach { subsequentPosition ->
-                        addAll(antiNodesLocationsForFrequency(currentPosition, subsequentPosition))
-                    }
-                }
+    fun calculateAntiNodes(current: Vec2, subsequent: Vec2, mapRange: MapRange) = buildSet<Vec2> {
+        val deltaX = current.x - subsequent.x
+        val deltaY = current.y - subsequent.y
+
+        Vec2(current.x + deltaX, current.y + deltaY).takeIf { position -> position in mapRange }?.let(::add)
+        Vec2(subsequent.x - deltaX, subsequent.y - deltaY).takeIf { position -> position in mapRange }?.let(::add)
+    }
+
+    fun calculateAntiNodesWithResonance(current: Vec2, subsequent: Vec2, mapRange: MapRange) = buildSet<Vec2> {
+        add(current)
+        val deltaX = current.x - subsequent.x
+        val deltaY = current.y - subsequent.y
+        var forward = current
+        var reverse = current
+        do {
+            forward = forward.copy(forward.x + deltaX, forward.y + deltaY)
+            reverse = reverse.copy(reverse.x - deltaX, reverse.y - deltaY)
+            if (forward in mapRange) add(forward)
+            if (reverse in mapRange) add(reverse)
+        } while (forward in mapRange || reverse in mapRange)
+    }
+
+    fun calculateAntiNodes(positions: List<Vec2>, mapRange: MapRange, strategy: Strategy) = buildSet<Vec2> {
+        positions.dropLast(1).forEachIndexed { index, current ->
+            val subsequentPositions = positions.slice(index + 1..positions.lastIndex)
+            subsequentPositions.forEach { subsequent ->
+                when (strategy) {
+                    Signal -> calculateAntiNodes(current, subsequent, mapRange)
+                    Resonance -> calculateAntiNodesWithResonance(current, subsequent, mapRange)
+                }.run(::addAll)
             }
-            antiNodesPositions
+        }
+    }
+
+    fun uniqueAntiNodesLocations(map: List<String>, strategy: Strategy): Int {
+        val mapRange = MapRange(map)
+        return frequencyLocationsMap(map).flatMap { (_, positions) ->
+            calculateAntiNodes(positions, mapRange, strategy)
         }.distinct().count()
-
-    fun part1(input: List<String>): Int = MapRange(input).let { mapRange ->
-        antiNodesLocationsInMap(input) { (thisX, thisY), (nextX, nextY) ->
-            val deltaX = thisX - nextX
-            val deltaY = thisY - nextY
-            val next = Vec2(thisX + deltaX, thisY + deltaY).takeIf { position -> position in mapRange }
-            val before = Vec2(nextX - deltaX, nextY - deltaY).takeIf { position -> position in mapRange }
-            setOfNotNull(next, before)
-        }
     }
 
-    fun part2(input: List<String>): Int = MapRange(input).let { mapRange ->
-        antiNodesLocationsInMap(input) { position, subsequentPosition ->
-            val frequencyPositions = mutableSetOf(position)
-            val deltaX = position.x - subsequentPosition.x
-            val deltaY = position.y - subsequentPosition.y
-            var forward = position
-            var reverse = position
-            do {
-                forward = forward.copy(forward.x + deltaX, forward.y + deltaY)
-                reverse = reverse.copy(reverse.x - deltaX, reverse.y - deltaY)
-                if (forward in mapRange) frequencyPositions.add(forward)
-                if (reverse in mapRange) frequencyPositions.add(reverse)
-            } while (forward in mapRange || reverse in mapRange)
-            frequencyPositions
-        }
-    }
+    fun part1(input: List<String>): Int = uniqueAntiNodesLocations(input, strategy = Signal)
+
+    fun part2(input: List<String>): Int = uniqueAntiNodesLocations(input, strategy = Resonance)
 
     val input = readInput("Day08")
     part1(input).println()
